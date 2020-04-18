e364bba593ad939d3bff6479ae7be55aa84f7736
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-04-17 22:59:17 -0300

    First routes created

---
+++ web/src/pages/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {} from './styles.js';
+
+export default function Main() {
+  return (
+    <h1>Main</h1>
+  );
+}
---
+++ web/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard/';
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Dashboard} exact />
+      </Switch>
+    </Router>
+  );
+}
