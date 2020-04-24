26f1f813bf24233736747c99f06a1dc6f5b34249
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-04-24 11:33:58 -0300

    Calendar finished

--- web/src/components/Calendar/index.js
+++ web/src/components/Calendar/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import zeroFill from 'zero-fill';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-import { Container, Title, Month, Day, Header, Names, Name } from './styles.js';
+import { Container, Title, Month, Day, Header, Names, Name, Footer, List, Item, Circle, NameForDay } from './styles.js';
 
 export default function Calendar() {
 	const [date, setDate] = useState(new Date());
@@ -10,6 +10,18 @@ export default function Calendar() {
 	const [month, setMonth] = useState(date.getMonth());
 	const [dates, setDates] = useState([]);
 	const week = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
+	const months = ["Janeiro",
+		"Fevereiro",
+		"Marco",
+		"Abril",
+		"Maio",
+		"Junho",
+		"Julho",
+		"Agosto",
+		"Septembro",
+		"Outubro",
+		"Novembro",
+		"Dezembro"]
 
 	function getDaysInMonth(monthGet, yearGet) {
 	 return new Date(yearGet, monthGet, 0).getDate();
@@ -17,11 +29,15 @@ export default function Calendar() {
 	
 
 	function handleLeft() {
-		setDate(new Date(year, month - 1, 1));
+		const newDate = new Date(year, month - 1, 1);
+		if(newDate.getFullYear() !== year) return;
+		setDate(newDate);
 	}
 
 	function handleRight() {
-		setDate(new Date(year, month + 1, 1));
+		const newDate = new Date(year, month + 1, 1);
+		if(newDate.getFullYear() !== year) return;
+		setDate(newDate);
 	}
 
 	useEffect(() => {
@@ -35,21 +51,34 @@ export default function Calendar() {
 
 	useEffect(() => {
 		const list = [];
-		const daysOldMonth = getDaysInMonth(month, year);
-		const days = daysOldMonth - (date.getDay()-1);
-		for(let counter = days;counter <= daysOldMonth;counter++) {
+		
+		// Calculate days of the month at this time
+		const dayLastThisTime = getDaysInMonth(month+1, year)
+
+		// Calculate days of the last month
+		const dayLastOldMonth = getDaysInMonth(month, year);
+		const daysOldMonthCount = dayLastOldMonth - (date.getDay()-1);
+
+		// days of the last month
+		for(let counter = daysOldMonthCount;counter <= dayLastOldMonth;counter++) {
 			const dateLocale = new Date(year, month - 1, counter);
-			list.push({ id: Math.random() * 5, value: counter, dateLocal: dateLocale, vef: true });
+			list.push({ id: Math.random() * 5, value: counter, dateLocale, vef: false, off: true });
 		}
-		for(let counter = 1;counter <= getDaysInMonth(month+1, year);counter++) {
-			const dateLocale = new Date(year, month, counter-3);
-			list.push({ id: counter, value: counter, dateLocal: dateLocale, vef: false });
+		
+		// Days of the month
+		for(let counter = 1;counter <= dayLastThisTime;counter++) {
+			const dateLocale = new Date(year, month, counter);
+			const dateWeek = dateLocale.getDay();
+			const vef = dateWeek === 0 || dateWeek === 6 || month+1 === 1 || month+1 === 6 || month+1 === 6 || month+1 === 7 || month+1 === 12;
+			list.push({ id: counter, value: counter, dateLocale, vef, off: false });
 		}
+		
 		setDates([...list]);
-	}, [year, month, date]);
+	}, [date, year, month]);
 	
 	return (
 		<Container>
+			<Title>Mês: {months[month].toLowerCase()}</Title>
 			<Header>
 				<FiChevronLeft onClick={handleLeft} color='#7159c1' size={24} />
 				<Title>{year} - {zeroFill(2, month+1)}</Title>
@@ -59,16 +88,37 @@ export default function Calendar() {
 				{week.map(day => <Name key={day}>{day}</Name>)}
 			</Names>
 			<Month>
-				{dates.map(({ id, vef, value, dateLocal }) => (
+				{dates.map(({ id, off, vef, value, dateLocale }) => (
 					<Day
 						key={id}
 						selected={(value === new Date().getDate()) ? true : false}
-						off={vef}
+						off={off}
+						vef={vef}
 					>
 						{value}
 					</Day>
 				))}
 			</Month>
+			<Footer>
+				<List>
+					<Item>
+						<Circle color="#4aff74" />
+						<NameForDay>Hoje</NameForDay>
+					</Item>
+					<Item>
+						<Circle color="#e02041" />
+						<NameForDay>Não letívo</NameForDay>
+					</Item>
+					<Item>
+						<Circle color="#bbb" />
+						<NameForDay>Mês anterior</NameForDay>
+					</Item>
+					<Item>
+						<Circle color="#333" />
+						<NameForDay>Letívo</NameForDay>
+					</Item>
+				</List>
+			</Footer>
 		</Container>
 	);
 }
--- web/src/components/Calendar/styles.js
+++ web/src/components/Calendar/styles.js
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	background: #f5f5f5;
 	margin: 0 25px;
-	padding: 20px;
+	padding: 5px 20px 20px 20px;
 `;
 
 export const Header = styled.header`
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
-	margin-top: -12.5px;
 	margin-bottom: 5px;
 
 	& > svg {
@@ -32,23 +31,34 @@ export const Month = styled.ul`
 	list-style: none;
 	display: grid;
 	grid-template-columns: repeat(7, 1fr);
+	grid-gap: 5px;
 `;
 
 export const Day = styled.li`
-	color: ${props => {
-		if(props.selected) return '#e02041';
-		if(props.off) return '#999';
-		return '#333';
-	}};
-	cursor: ${props => props.selected ? 'normal' : 'pointer'};
+	color: ${props => (props.selected) ? '#4aff74'
+		: (props.off) ? '#999'
+		: (props.vef) ? '#e02041'
+		: '#333'};
+	cursor: ${props => (props.selected) ? 'normal'
+		: 'pointer'};
+	font-weight: ${props => (props.selected) ? 'bold'
+		: (props.vef) ? 'bold'
+		: 'normal'};
 	text-align: center;
-	border-radius: 2px;
-	border: 1px solid ${props => props.selected ? 'red' : 'transparent'};
-	transition: .2s border, .2s color;
+	border-radius: 4px;
+	border: ${props => props.selected ? '2px solid green'
+		: props.vef ? '2px solid transparent'
+		: '1px solid transparent'};
+	transition: .2s filter;
 	
 	&:hover {
-		border: 1px solid ${props => props.selected ? 'red' : '#444'};
-		color: ${props => props.selected ? '#e02041' : 'lightblue'};
+		filter: brightness(80%);
+		border: ${props => props.selected ? '2px solid #4aff74'
+			: props.vef ? '2px solid #e02041'
+			: '1px solid #444'};
+		color: ${props => props.selected ? '#4aff74'
+			: props.vef ? '#e02041'
+			: 'lightblue'};
 	}
 `;
 
@@ -60,4 +70,40 @@ export const Names = styled.ul`
 `;
 
 export const Name = styled.li`
+	color: #333;
+	font-weight: 600;
+`;
+
+export const Footer = styled.nav`
+	display: flex;
+`;
+
+export const List = styled.ul`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-gap: 0 50px;
+	list-style: none;
+	margin: 15px 15px 0 20px;
+`;
+
+export const Item = styled.li`
+	display: flex;
+	flex-direction: row;
+`;
+
+
+export const Circle = styled.div`
+	width: 7.5px;
+	height: 7.5px;
+	background: ${props => props.color ? props.color
+		: '#333'};
+	margin: 5px 7.5px 0 7.5px;
+	border-radius: 50%;
+`;
+
+export const NameForDay = styled.label`
+	font-family: 'Roboto', sans-serif;
+	font-size: 14px;
+	color: #333;
+	font-weight: 600;
 `;
